dcd6362e56849a2776424dfad6a2d79d977f73bf
BrhayanLS <87352660+BrhayanLS@users.noreply.github.com>
2022-12-14 20:23:00 -0500

    Se crea la carpate para los controladores

---
+++ src/main/java/com/my/paralon/controllers/controllerRegistroCliente.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.my.paralon.controllers;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author brhay
+ */
+@Named(value = "controllerRegistroCliente")
+@SessionScoped
+public class controllerRegistroCliente implements Serializable {
+
+    /**
+     * Creates a new instance of controllerRegistroCliente
+     */
+    public controllerRegistroCliente() {
+    }
+    
+}
